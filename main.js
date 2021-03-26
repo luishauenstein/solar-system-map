8481be673594050806b5422a164f8fe4f1958d20
Luis Hauenstein <account@luishauenstein.com>
2021-03-26 15:09:40 +0100

    three.js is now self-hosted and not fetched from a cdn anymore

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import * as THREE from "./node_modules/three/build/three.module.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -6,14 +8,35 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+/*
+const light = new THREE.PointLight(0xff0000, 1, 100);
+light.position.set(0, 0, 0);
+scene.add(light);
+*/
+
+/*
+const loader = new GLTFLoader();
+loader.load(
+  "./lowpolyplanets/sun/model.gltf",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+*/
+
+//sample cube:
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-camera.position.x = 5;
-camera.position.y = 5;
+camera.position.z = 10;
+camera.position.x = 10;
+camera.position.y = 10;
 camera.lookAt(0, 0, 0);
 
 function animate() {
