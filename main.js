5559be941f36c7ca208515dc53a290ad04fd1fd7
Luis Hauenstein <account@luishauenstein.com>
2021-04-12 00:30:34 +0200

    light experiments

--- main.js
+++ main.js
@@ -11,39 +11,47 @@ const scene = new THREE.Scene();
 
 //init camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(250, 0, 0);
+camera.position.set(500, 0, 0);
 camera.lookAt(0, 0, 0);
 
 //orbit controls for camera
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const light = new THREE.PointLight();
-light.position.set(100, 50, 50);
-scene.add(light);
+//LIGHT EXPERIMENTS
+scene.add(new THREE.AmbientLight(0x101010));
 
-/*
-let obj;
-let x = 5;
-async function load() {
-  const loader = new GLTFLoader();
-  await loader
-    .load("./models/earth.gltf", function (gltf) {
-      obj = gltf.scene;
-      obj.position.set(0, 0, 0);
-      obj.rotation.x = 120;
-      scene.add(obj);
-      console.log(obj);
-    })
-    .resolve((i) => console.log(i));
-}
-load();
-setTimeout(() => console.log(obj), 1000);
-*/
+const lightdist = 110;
+const strength = 3;
+const light1 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light1.position.set(lightdist, 0, 0);
+scene.add(light1);
+
+const light2 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light2.position.set(-lightdist, 0, 0);
+scene.add(light2);
+
+const light3 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light3.position.set(0, lightdist, 0);
+scene.add(light3);
+
+const light4 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light4.position.set(0, -lightdist, 0);
+scene.add(light4);
+
+const light5 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light5.position.set(0, 0, lightdist);
+scene.add(light5);
+
+const light6 = new THREE.PointLight(0xffffff, strength, 0, 2);
+light6.position.set(0, 0, -lightdist);
+scene.add(light6);
 
 class CelestialObject {
-  constructor(name) {
+  constructor(name, rotSpeed = 0.005, tilt = 0.5) {
     this.name = name;
     this.url = `./models/${name}.gltf`;
+    this.rotSpeed = rotSpeed;
+    this.tilt = tilt;
     this.LoadGLTF();
   }
 
@@ -55,24 +63,24 @@ class CelestialObject {
   _OnLoaded(obj) {
     this.transform = obj;
     this.transform.position.set(0, 0, 0);
-    this.transform.rotation.x = 0.5;
+    this.transform.rotation.x = this.tilt;
     scene.add(this.transform);
+  }
+
+  rotate(factor = 1) {
+    this.transform && (this.transform.rotation.y += this.rotSpeed * factor);
   }
 }
 
 const earth = new CelestialObject("earth");
-setTimeout(() => earth.transform.position.set(100, 0, 0), 1000);
-const sun = new CelestialObject("sun");
-
-const rotate = (co) => {
-  co.transform && (co.transform.rotation.y += 0.005);
-};
+setTimeout(() => earth.transform.position.set(350, 0, 0), 1000);
+const sun = new CelestialObject("sun", 0.001);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  rotate(earth);
-  rotate(sun);
+  sun.rotate();
+  earth.rotate();
 
   renderer.render(scene, camera);
 }
