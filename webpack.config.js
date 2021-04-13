2edea924d046c62b1adfb098042aecc37e2ee5bb
Luis Hauenstein <account@luishauenstein.com>
2021-04-13 14:04:36 +0200

    webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })],
   },
 };
