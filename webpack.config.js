d9297288832e717b17a77189aa5a1167c64db193
Luis Hauenstein <account@luishauenstein.com>
2021-04-13 14:55:35 +0200

    set up webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
+  devServer: {
+    //IMPORTANT: dev server displays webgl content very blurry, quality is better in production build
+    contentBase: "./dist",
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
