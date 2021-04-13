24b4a08fa2456f12ab7b5413031cb897c4ce5e8f
Luis Hauenstein <account@luishauenstein.com>
2021-04-13 14:43:45 +0200

    added asset copy to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   optimization: {
     minimizer: [new TerserPlugin({ extractComments: false })],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/models", to: "models" }],
+    }),
+  ],
 };
