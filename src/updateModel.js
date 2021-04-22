5cf7e05066fb82759811e0c6cf0761d8b9e43d6f
Luis Hauenstein <account@luishauenstein.com>
2021-04-22 17:52:37 +0200

    datetime display box working

--- src/updateModel.js
+++ src/updateModel.js
@@ -1,10 +1,31 @@
-//updates model fitting to datetime
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+//ui elements
+const datetimeContainer = document.getElementById("datetime");
+
+//updates simulation by datetime
 const updateModel = (planetaryObjects, datetime) => {
+  //update planet position & rotate them
   for (const planet of planetaryObjects) {
     planet.SetPositionByDate(datetime);
     planet.Rotate();
   }
+
+  //update UI
+  datetimeContainer.innerHTML = `${monthNames[datetime.getMonth()]} ${datetime.getYear() + 1900}`;
 };
 
 export default updateModel;
